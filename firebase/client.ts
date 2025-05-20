94beee4dd151d5b75b15ac662d8151f3be64e269
Kerim Imamović <imamovic.kerim@gmail.com>
2025-05-20 17:42:36 +0200

    Connect Next JS to Firebase

---
+++ firebase/client.ts
@@ -0,0 +1,29 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { type Auth, getAuth } from 'firebase/auth';
+import { type FirebaseStorage, getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'udemy-fire-homes-project.firebaseapp.com',
+  projectId: 'udemy-fire-homes-project',
+  storageBucket: 'udemy-fire-homes-project.firebasestorage.app',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const currentApps = getApps();
+
+let auth: Auth;
+let storage: FirebaseStorage;
+
+if (!currentApps.length) {
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  storage = getStorage(app);
+} else {
+  const app = currentApps[0];
+  auth = getAuth(app);
+  storage = getStorage(app);
+};
+
+export { auth, storage };
