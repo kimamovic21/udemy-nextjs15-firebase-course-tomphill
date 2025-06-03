90a35d24025f9f3b897f12cb42bb5e0e429b08fb
Kerim Imamović <imamovic.kerim@gmail.com>
2025-06-03 11:20:39 +0200

    Create the register user server action

--- app/(auth)/register/register-form.tsx
+++ app/(auth)/register/register-form.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { registerUserSchema } from '@/validation/registerUser';
+import { registerUser } from './actions';
 import {
   Form,
   FormControl,
@@ -15,34 +19,11 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import ContinueWithGoogleButton from '@/components/continue-with-google-button';
 
-const formSchema = z.object({
-  name: z
-    .string()
-    .min(2, 'Name must be at least 2 characters')
-    .max(20, 'Name must be at most 20 characters'),
-  email: z
-    .string()
-    .email('Invalid email'),
-  password: z.string().refine((value) => {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-    return regex.test(value);
-  }, {
-    message: 'Password must contain at least 6 characters, at least one uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
-  }),
-  passwordConfirm: z.string(),
-}).superRefine((data, ctx) => {
-  if (data.password !== data.passwordConfirm) {
-    ctx.addIssue({
-      message: 'Passwords do not match',
-      path: ['passwordConfirm'],
-      code: 'custom',
-    });
-  };
-});
-
 const RegisterForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof registerUserSchema>>({
+    resolver: zodResolver(registerUserSchema),
     defaultValues: {
       name: '',
       email: '',
@@ -51,8 +32,21 @@ const RegisterForm = () => {
     },
   });
 
-  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+  const handleSubmit = async (data: z.infer<typeof registerUserSchema>) => {
+    const response = await registerUser(data);
 
+    if (!!response?.error) {
+      toast('Error!', {
+        description: response?.message,
+      });
+      return;
+    };
+
+    toast('Success!', {
+      description: 'Your account was created successfully',
+    });
+
+    router.push('/login');
   };
 
   return (
