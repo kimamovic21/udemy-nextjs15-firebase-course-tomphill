5d404f3864cd79256bb89b0275af1787013424f7
Kerim Imamović <imamovic.kerim@gmail.com>
2025-06-10 16:12:11 +0200

    Render the list of favorites in the my-favorites page

--- app/account/my-favorites/page.tsx
+++ app/account/my-favorites/page.tsx
@@ -1,5 +1,17 @@
 import { getUserFavorites } from '@/data/favorites';
 import { getPropertiesById } from '@/data/properties';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import PropertyStatusBadge from '@/components/property-status-badge';
+import { EyeIcon, Trash2Icon } from 'lucide-react';
+import Link from 'next/link';
 
 const MyFavoritesPage = async ({
   searchParams
@@ -19,10 +31,79 @@ const MyFavoritesPage = async ({
   );
 
   const properties = await getPropertiesById(paginatedFavorites);
-  console.log({ paginatedFavorites, properties });
 
   return (
-    <div>MyFavoritesPage</div>
+    <div className='max-w-screen-lg mx-auto'>
+      <h2 className='text-4xl font-bold py-4 mt-5'>
+        My favorites
+      </h2>
+
+      {!paginatedFavorites.length && (
+        <h3 className='text-center text-zinc-400 text-3xl font-bold py-10'>
+          You have no favorite properties
+        </h3>
+      )}
+
+      {!!paginatedFavorites.length && (
+        <Table className='mt-4'>
+          <TableHeader>
+            <TableRow>
+              <TableHead>
+                Property
+              </TableHead>
+              <TableHead>
+                Status
+              </TableHead>
+              <TableHead className='text-right'>
+                View/Delete
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {paginatedFavorites?.map((favorite) => {
+              const property = properties?.find((property) => property.id === favorite);
+              const address = [
+                property?.address1,
+                property?.address2,
+                property?.city,
+                property?.postcode,
+              ].filter((addressLine) => !!addressLine).join(', ');
+
+              return (
+                <TableRow key={favorite}>
+                  <TableCell>
+                    {address}
+                  </TableCell>
+
+                  <TableCell>
+                    {!!property && (
+                      <PropertyStatusBadge status={property?.status} />
+                    )}
+                  </TableCell>
+
+                  <TableCell className='flex justify-end gap-1'>
+                    {!!property && (
+                      <>
+                        <Button asChild variant='outline'>
+                          <Link href={`/property/${property?.id}`}>
+                            <EyeIcon />
+                          </Link>
+                        </Button>
+
+                        <Button variant='outline'>
+                          <Trash2Icon />
+                        </Button>
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      )}
+    </div>
   );
 };
 
